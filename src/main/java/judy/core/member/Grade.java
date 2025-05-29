9074393c9bec625bf8cf8cacb2ddc35993e63685
d225006 <yeonju2688.acu@gmail.com>
2025-05-29 14:01:14 +0900

    init : 스켈레톤 프로젝트 commit

---
+++ src/main/java/judy/core/member/Grade.java
@@ -0,0 +1,10 @@
+package judy.core.member;
+
+/**
+ * 회원 등급
+ */
+public enum Grade {
+
+    BASIC,
+    VIP
+}
---
+++ src/test/java/judy/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package judy.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
