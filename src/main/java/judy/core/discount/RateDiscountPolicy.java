2fa9a47ac7cc64aeadf5bd6df83ae3003009148a
JudyYeon <dev.judykim@gmail.com>
2025-06-04 23:16:05 +0900

    func : 새로운 할인정책을 적용하고 테스트 코드를 작성

---
+++ src/main/java/judy/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package judy.core.discount;
+
+import judy.core.member.Grade;
+import judy.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP){
+            return price * discountPercent / 100;
+        }
+        return 0;
+    }
+}
