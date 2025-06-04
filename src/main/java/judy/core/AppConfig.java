f29af369db91f8c81aba9ecd4d59edcf2aae4a08
JudyYeon <dev.judykim@gmail.com>
2025-06-04 23:59:47 +0900

    refactor : 생성자 주입방식으로 변경

--- src/main/java/judy/core/AppConfig.java
+++ src/main/java/judy/core/AppConfig.java
@@ -1,13 +1,20 @@
 package judy.core;
 
+import judy.core.discount.FixDiscountPolicy;
 import judy.core.member.MemberService;
 import judy.core.member.MemberServiceImpl;
 import judy.core.member.MemoryMemberRepository;
+import judy.core.order.OrderService;
+import judy.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
     public MemberService memberService(){
         return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
     }
 
 
--- src/main/java/judy/core/order/OrderServiceImpl.java
+++ src/main/java/judy/core/order/OrderServiceImpl.java
@@ -1,16 +1,28 @@
 package judy.core.order;
 
 import judy.core.discount.DiscountPolicy;
-import judy.core.discount.FixDiscountPolicy;
 import judy.core.member.Member;
 import judy.core.member.MemberRepository;
-import judy.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
     // final = 반드시 값을 할당해야함. null 금지
     // 인터페이스 - 구현체 조합으로 의존
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private MemberRepository memberRepository;
+
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 문제발생 구간..!
+
+    // 새로운 할인 정책이 나왔다면 OrderServiceImpl 소스를 변경해야한다.
+    //  private final DiscountPolicy discountPolicy = new 이 부분에 변경되는 정책 구현체();
+    // 역할과 구현을 충실하게 분리했는가?
+    // 다형성도 활용하고, 인터페이스와 구현객체를 분리했는가? -- No
+    // 추상인터페이스에 의존하지만 구현 클래스에도 의존하고 있음... 기능확장시 클라이언트 코드에 영향을 준다 > OCP 위반
+    private DiscountPolicy discountPolicy;
+
+    // 생성자
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     /**
      * 주문 생성하기
