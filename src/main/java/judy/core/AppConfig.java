c8bb8651ac0708dd2ca50690b5f5a25119aea9ea
d225006 <yeonju2688.acu@gmail.com>
2025-06-05 12:50:10 +0900

    refactor : 메서드명에서 역할이 드러나도록 리팩토링

--- src/main/java/judy/core/AppConfig.java
+++ src/main/java/judy/core/AppConfig.java
@@ -1,6 +1,8 @@
 package judy.core;
 
+import judy.core.discount.DiscountPolicy;
 import judy.core.discount.FixDiscountPolicy;
+import judy.core.member.MemberRepository;
 import judy.core.member.MemberService;
 import judy.core.member.MemberServiceImpl;
 import judy.core.member.MemoryMemberRepository;
@@ -9,12 +11,22 @@ import judy.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    // 메서드 명에서 역할이 드러나도록 해야함
+
     public MemberService memberService(){
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
+    private MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService(){
         return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
 
