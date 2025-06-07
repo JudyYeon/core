dc88d19098968ed780129ec3e112ee94d498577f
JudyYeon <dev.judykim@gmail.com>
2025-06-07 12:21:25 +0900

    add : DI 컨테이너 주석

--- src/main/java/judy/core/AppConfig.java
+++ src/main/java/judy/core/AppConfig.java
@@ -8,24 +8,36 @@ import judy.core.member.MemberServiceImpl;
 import judy.core.member.MemoryMemberRepository;
 import judy.core.order.OrderService;
 import judy.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+/**
+ * DI 컨테이너
+ * 객체를 생성하고 관리하면서 의존관계를 연결한다.
+ * = 오브젝트 팩토리로 불리기도 함.
+ */
+@Configuration
 public class AppConfig {
 
     // 메서드 명에서 역할이 드러나도록 해야함
     // 단축키 cmd + e = 이전 히스토리 보여짐
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository(){
+    @Bean
+    public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new FixDiscountPolicy();
     }
