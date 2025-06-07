a5e456f7f3903eb0cf6550acafe45c585c572bd7
JudyYeon <dev.judykim@gmail.com>
2025-06-07 12:29:48 +0900

    refactor : DI 컨테이너를 수동으로 적용하지 않고, 스프링(어노테이션)을 이용하여 프레임워크에 제어를 역전함

--- src/main/java/judy/core/MemberApp.java
+++ src/main/java/judy/core/MemberApp.java
@@ -3,13 +3,20 @@ package judy.core;
 import judy.core.member.Grade;
 import judy.core.member.Member;
 import judy.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // AnnotationConfigApplicationContext 안에 설정파일을 적어준다. 스프링 어노테이션들이 찾아옴
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // 기본 : 메서드명, 빈으로 등로간 것을 찾아와줌
+
         Member member = new Member(1L, "memberA", Grade.VIP); // ctrl + alt + v = new Member에 대한 정의를 자동완성
         memberService.join(member);
 
