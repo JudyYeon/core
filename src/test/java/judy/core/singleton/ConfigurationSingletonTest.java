c6b6c78a974e20ab9efb8efb2ef1356001d8ef3a
JudyYeon <dev.judykim@gmail.com>
2025-06-08 16:43:58 +0900

    test : 컨테이너는 싱글톤을 보장함

---
+++ src/test/java/judy/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,33 @@
+package judy.core.singleton;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import judy.core.AppConfig;
+import judy.core.member.MemberRepository;
+import judy.core.member.MemberServiceImpl;
+import judy.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    @DisplayName("설정 bean 싱글톤 검증")
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        //모두 같은 인스턴스를 참고하고 있다.
+        System.out.println("memberService -> memberRepository = " + memberService.getMemberRepository());
+        System.out.println("orderService -> memberRepository  = " + orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);
+        //모두 같은 인스턴스를 참고하고 있다.
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
