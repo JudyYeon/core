a594b2986279885e1c3990d4adcc20b8bdcb5531
JudyYeon <dev.judykim@gmail.com>
2025-06-08 16:04:21 +0900

    test : 싱글톤을 무상태로 설계해야하는 이유

---
+++ src/test/java/judy/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,42 @@
+package judy.core.singleton;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class StatefulServiceTest {
+
+    @Test
+    @DisplayName("싱글톤에서 상태유지시 문제가 되는 이유")
+     public void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+        //ThreadA: A사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+        //ThreadB: B사용자 20000원 주문
+        statefulService2.order("userB", 20000);
+
+        //ThreadA: 사용자A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        //ThreadA: 사용자A는 10000원을 기대했지만, 기대와 다르게 20000원 출력
+        System.out.println("price = " + price);
+        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    /**
+     * 테스트용 DI 컨테이너
+     */
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+
+}
