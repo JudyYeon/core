31270994eb4bc7fe84e9c3850d76594fa8121564
JudyYeon <dev.judykim@gmail.com>
2025-06-08 19:01:42 +0900

    add : 빈을 자동으로 등록하도록 하는 설정

---
+++ src/test/java/judy/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,22 @@
+package judy.core.scan;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import judy.core.AutoAppConfig;
+import judy.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+
+    @Test
+    @DisplayName("AutoAppConfig 를 이용한 자동 스캔")
+    void basicScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
