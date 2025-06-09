0196e90386067ca13f0bd0f764dad50020486ded
d225006 <yeonju2688.acu@gmail.com>
2025-06-09 14:42:59 +0900

    feat : 커스텀 어노테이션(컴포넌트) 만들어 의도적으로 bean exclude 테스트를 진행한다.

--- src/test/java/judy/core/scan/filter/ComponentFilterAppConfigTest.java
+++ src/test/java/judy/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -1,11 +1,38 @@
 package judy.core.scan.filter;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.context.annotation.ComponentScan.*;
 
 public class ComponentFilterAppConfigTest {
 
     @Test
     void filterScan(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
 
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+//        BeanB beanB = ac.getBean("beanB", BeanB.class);
+
+        Assertions.assertThat(beanA).isNotNull();
+        assertThrows(NoSuchBeanDefinitionException.class, () ->
+                ac.getBean("beanB", BeanB.class));
     }
+
+    /**
+     * 나만의 컴포넌트 스캔하기
+     */
+    @Configuration
+    @ComponentScan(
+            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    ) // 단축키 option + enter = static import 하기
+    static class ComponentFilterAppConfig{ }
 }
