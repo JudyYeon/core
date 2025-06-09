321ef54c1a729cc6321d0131a590f26fea3f2621
d225006 <yeonju2688.acu@gmail.com>
2025-06-09 15:12:22 +0900

    test : 자동 주입 bean 과 수동 주입 bean 이 같을 경우 테스트

--- src/main/java/judy/core/AutoAppConfig.java
+++ src/main/java/judy/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package judy.core;
 
+import judy.core.member.MemberRepository;
+import judy.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,4 +13,10 @@ import org.springframework.context.annotation.FilterType;
         // @Configuration 에 이미 @Component 되어있어서 제외시켜준다(충돌방지) 예제에서만 이렇게 씀
 )
 public class AutoAppConfig {
+
+    // 자동 주입 bean 과 수동 주입 bean 이 같을 경우 테스트 = 수동 주입이 우선임
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
