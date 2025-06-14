d7f3dc1dda5c7229343d064dacafeafa055867a9
JudyYeon <dev.judykim@gmail.com>
2025-06-14 20:20:11 +0900

    test : InitializingBean, DisposableBean 스프링 빈이 스프링 코드에 의존하지 않고 빈등록 초기화, 소멸 메서드 지정하는 방법

--- src/test/java/judy/core/lifecycle/NetworkClient.java
+++ src/test/java/judy/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package judy.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -34,10 +31,8 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 
     /**
      * 의존 관계 주입이 끝나면 호출하는 메서드
-     * @throws Exception
      */
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
@@ -45,10 +40,8 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 
     /**
      * 메서드 소멸
-     * @throws Exception
      */
-    @Override
-    public void destroy() throws Exception {
+    public void close()  {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
