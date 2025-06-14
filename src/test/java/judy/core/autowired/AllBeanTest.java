a8060d27ad6a385da05b9e7e1418eceb6079d28b
JudyYeon <dev.judykim@gmail.com>
2025-06-14 11:23:18 +0900

    test : Bean 이름 객체를 활용하여 할인정책의 유형을 찾고 추상화된 객체의 다형성을 이용하여 할인 금액을 계산

--- src/test/java/judy/core/autowired/AllBeanTest.java
+++ src/test/java/judy/core/autowired/AllBeanTest.java
@@ -1,9 +1,13 @@
 package judy.core.autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 import java.util.Map;
 import judy.core.AutoAppConfig;
 import judy.core.discount.DiscountPolicy;
+import judy.core.member.Grade;
+import judy.core.member.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,6 +18,19 @@ public class AllBeanTest {
     void findAllBean() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
 
+        // Bean 에서 할인 로직 객체를 가져온다.
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        // 멤버 객체 생성
+        Member member = new Member(1L, "userA", Grade.VIP);
+        // 할인금액 계산
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+
+        assertThat(rateDiscountPrice).isEqualTo(2000);
 
     }
 
@@ -33,5 +50,17 @@ public class AllBeanTest {
             System.out.println("policies = " + policies);
         }
 
+        /**
+         * 할인 서비스
+         * @return 할인 금액
+         */
+        public int discount(Member member, int price, String discountCode) {
+            // Bean Map 에서 빈 이름과 동일한 할인 정책 객체를 가져온다.
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            System.out.println("discountCode = " + discountCode);
+            System.out.println("discountPolicy = " + discountPolicy);
+
+            return discountPolicy.discount(member, price);
+        }
     }
 }
