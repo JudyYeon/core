33655d83393decd2923f0b13c54f0e96d1d94241
JudyYeon <dev.judykim@gmail.com>
2025-06-14 21:16:42 +0900

    test : 프로토타입 스코프를 스프링 컨테이너에 조회하면 스프링 컨테이너는 항상 새로운 인스턴스를 생성해서 반환함

---
+++ src/test/java/judy/core/scope/PrototypeTest.java
@@ -0,0 +1,48 @@
+package judy.core.scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeTest() {
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close();
+
+    }
+
+    @Scope("prototype")
+    // @Component 없는데도 Bean 등록이 되는 이유는 ?
+    // new AnnotationConfigApplicationContext(PrototypeBean.class); 에서 직접 지정해주었기 때문에 스캔 대상이됨
+    static class PrototypeBean{
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destory");
+        }
+
+    }
+}
