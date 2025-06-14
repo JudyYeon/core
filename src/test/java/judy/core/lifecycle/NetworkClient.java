b2230b02b3241dab8ddd8d756a1c118e9ed4033c
JudyYeon <dev.judykim@gmail.com>
2025-06-14 20:14:03 +0900

    test : InitializingBean, DisposableBean 를 활용하여 생성자 주입 후 안전하게 초기화, DI컨테이너 종료 전 연결 종료 close 안전하게 호출

--- src/test/java/judy/core/lifecycle/NetworkClient.java
+++ src/test/java/judy/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package judy.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
@@ -9,8 +12,6 @@ public class NetworkClient {
      */
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -31,4 +32,24 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    /**
+     * 의존 관계 주입이 끝나면 호출하는 메서드
+     * @throws Exception
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    /**
+     * 메서드 소멸
+     * @throws Exception
+     */
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
 }
