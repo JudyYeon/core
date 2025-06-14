e8cdb54274cc8e69581cab78903ab80ecea79999
JudyYeon <dev.judykim@gmail.com>
2025-06-14 11:04:45 +0900

    test : DiscountService 클래스만 Bean 으로 등록했기 때문에 생성자에 주입된 값이 없음

---
+++ src/test/java/judy/core/autowired/AllBeanTest.java
@@ -0,0 +1,35 @@
+package judy.core.autowired;
+
+import java.util.List;
+import java.util.Map;
+import judy.core.discount.DiscountPolicy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(DiscountService.class);
+
+
+    }
+
+    /**
+     * 빈주입을 위해 테스트용 서비스 추가
+     */
+    static class DiscountService {
+
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+    }
+}
