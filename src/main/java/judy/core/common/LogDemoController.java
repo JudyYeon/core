b821caecf154b71f175860a5221b33b556c233b1
d225006 <dev.judykim@gmail.com>
2025-06-17 16:56:41 +0900

    fix : 스프링컨테이너 Bean 생성시점에 MyLogger Bean 을 주입받을 수 없으므로 (@Autowired 할수없음) 서버 error 오류발생

--- src/main/java/judy/core/common/LogDemoController.java
+++ src/main/java/judy/core/common/LogDemoController.java
@@ -6,10 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 로거 실행 테스트 컨트롤러
+ */
 @Controller // web 관련 의존성 gradle 추가했기 때문에 사용가능
-@RequiredArgsConstructor
+@RequiredArgsConstructor // 기본 필수 생성자 -> @Autowired Bean 주입
 public class LogDemoController {
 
+    // Autowired 되어있으므로 생성된 Bean 주입하여 final 가능
     private final LogDemoService logDemoService;
     private final MyLogger myLogger;
 
