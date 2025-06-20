d597389cf887c70cbd8927f2f2f49ee6e9b008c8
JudyYeon <dev.judykim@gmail.com>
2025-06-20 21:38:53 +0900

    refactor :  ObjectProvider 대신 proxyMode를 사용하여 프록시 객체로 마치 싱글톤 빈을 사용하듯이 편리하게 request scope를 사용함(웹 스코프가 아니어도 프록시는 사용할 수 있음)

--- src/main/java/judy/core/common/LogDemoService.java
+++ src/main/java/judy/core/common/LogDemoService.java
@@ -1,7 +1,6 @@
 package judy.core.common;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,11 +13,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    private final MyLogger myLogger;
 
 
     public void logic(String id) {
-        MyLogger myLogger = myLoggerObjectProvider.getObject();
+//        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
